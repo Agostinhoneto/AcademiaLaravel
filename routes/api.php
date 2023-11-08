a3f0f73deb71ec458f66dc457e13b82114151727
Agostinho <agostneto6@gmail.com>
2023-11-08 13:09:29 -0300

    primeiro commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\TreinosController;
+use App\Http\Controllers\GruposMuscularesController;
+use App\Http\Controllers\InstrutoresController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +24,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::controller(TreinosController::class)->group(function () {
+    Route::post('logout', 'logout')->name('auth_logout');
+    Route::post('refresh', 'refresh')->name('refresh');
+    Route::post('register', 'register')->middleware('AdminMiddleware');
+    Route::get('get_user', 'get_user')->name('refresh');
+    Route::get('index','index')->name('index.auth');
+    Route::get('show/{id}','show')->name('show.auth');
+    Route::put('update/{id}','update')->name('update.auth');
+    Route::delete('destroy/{id}','destroy')->name('destroy.auth');
+});
+
+
+Route::controller(InstrutoresController::class)->group(function () {
+    Route::post('logout', 'logout')->name('auth_logout');
+    Route::post('refresh', 'refresh')->name('refresh');
+    Route::post('register', 'register')->middleware('AdminMiddleware');
+    Route::get('get_user', 'get_user')->name('refresh');
+    Route::get('index','index')->name('index.auth');
+    Route::get('show/{id}','show')->name('show.auth');
+    Route::put('update/{id}','update')->name('update.auth');
+    Route::delete('destroy/{id}','destroy')->name('destroy.auth');
+});
+
+
+Route::controller(GruposMuscularesController::class)->group(function () {
     Route::post('logout', 'logout')->name('auth_logout');
     Route::post('refresh', 'refresh')->name('refresh');
     Route::post('register', 'register')->middleware('AdminMiddleware');
