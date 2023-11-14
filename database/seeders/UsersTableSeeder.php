a0a975a72a19224c6d1b364848a0b99b255c43d7
Agostinhoneto <agostneto6@gmail.com>
2023-11-14 13:49:34 -0300

    criando seeder user

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,6 +16,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+            // Inserir um usuário de exemplo
+            DB::table('users')->insert([
+                'name' => 'Agostinho',
+                'email' => 'agostneto6@gmail.com',
+                'password' => Hash::make('123'),
+            ]);        
     }
 }
